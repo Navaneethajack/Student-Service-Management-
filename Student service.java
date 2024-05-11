b18391dec4d623a734dc470aa12cc84a1e376727
Navaneetha Krishnan.S <140801207+Navaneethajack@users.noreply.github.com>
2024-05-12 05:20:14 +0530

    Create Student service.java

---
+++ Student service.java
@@ -0,0 +1,8 @@
+package service;
+
+public class StudentService {
+    public void enrollStudent(Student student, Course course) {
+        // Logic to enroll student in course (e.g., update database)
+        System.out.println("Enrolling " + student.getName() + " in " + course.getName() + " (placeholder implementation)");
+    }
+}
